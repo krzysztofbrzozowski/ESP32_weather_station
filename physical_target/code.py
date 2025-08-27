1257a23609cc041312957e4e877ffad208ee1399
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2025-08-27 21:51:10 +0200

    Displaying values on screen working

--- physical_target/code.py
+++ physical_target/code.py
@@ -1,15 +1,28 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-# Updated for Circuit Python 9.0
+# Updated for Circuit Python 10.0 b2
 
 import os
-
-import adafruit_connection_manager
 import wifi
+import zlib
+import json
+import time
 
 import adafruit_requests
+import adafruit_connection_manager
+from adafruit_datetime import datetime, timedelta
+from adafruit_magtag.magtag import MagTag
 
-import zlib
+magtag = MagTag()
+
+#Format text_to_display 
+magtag.add_text(
+    text_scale=1,
+    # text_wrap=25,
+    text_maxlen=300,
+    text_position=(10, 10),
+    text_anchor_point=(0, 0),
+)
 
 # Get ApiKey, WiFi details, ensure these are setup in settings.toml
 APIKEY = os.getenv("API_KEY")
@@ -22,13 +35,13 @@ ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl_context)
 rssi = wifi.radio.ap_info.rssi
 
-
 # URL for GET request
 JSON_GET_URL = f'https://airapi.airly.eu/v2/measurements/location?locationId=9910&apikey={APIKEY}&Accept-Encoding=gzip'
 # Define a custom header as a dict.
 
-print(f"\nConnecting to {SSID}...")
+# print(f"\nConnecting to {SSID}...")
 print(f"Signal Strength: {rssi}")
+
 try:
     # Connect to the Wi-Fi network
     wifi.radio.connect(SSID, PASSWORD)
@@ -36,27 +49,48 @@ except OSError as e:
     print(f"❌ OSError: {e}")
 print("✅ Wifi!")
 
-# Define a custom header as a dict.
+# Define a custom header as a dict
 HEADERS = {'AuhtKeys': 'auth_values'}
-# Add the gzip header
 HEADERS['Accept-Encoding'] = 'gzip'
 HEADERS['Accept'] = 'application/json'
 
 # print(f" | Fetching URL {JSON_GET_URL}")
 
-# Use with statement for retreiving GET request data
-with requests.get(JSON_GET_URL, headers=HEADERS) as response:
-    print(response)
+def get_data():
+    with requests.get(JSON_GET_URL, headers=HEADERS) as r:
+        assert r.status_code == 200, \
+            f'Expected 200 but got {r.status_code}'
 
-    if response.status_code == 200:
-        print(f" | 🆗 Status Code: {response.status_code}")
-    else:
-        print(f" | ❌ Status Code: {response.status_code}")
+        return r.content
 
-    with open("/sd/response.gz", "wb") as f:
-        f.write(response.content)
+def deflate_data(compressed_content):
+    decompressed = zlib.decompress(compressed_content, 31)
+    decompressed = decompressed.decode("utf-8")
 
-    # Decompressing data
-    data = response
-    decompressed = zlib.decompress(data, 31)
-    print(decompressed.decode("utf-8"))
+    try:
+        data = json.loads(decompressed)
+        iso_date_string = data['current']['tillDateTime']
+        # Adding time offset since hour from sensor is delayed by 2hrs
+        current_time_isodate = datetime.fromisoformat(iso_date_string) + timedelta(hours=2)
+        current_values = data['current']['values']
+
+        text_to_display = ""
+
+        t = current_time_isodate
+        text_to_display += f"{t.hour}:{t.minute} {t.day}.{t.month}.{t.year}\n"
+
+        for item in current_values:
+            text_to_display += f"{item['name']}: {item['value']}\n"
+
+        print(text_to_display)
+
+        magtag.set_text(text_to_display)
+        magtag.refresh()
+
+    except Exception as e:
+        print(e)
+
+while True:
+    content = get_data()
+    deflate_data(content)
+    time.sleep(60 * 10)
--- vrtual_target/main2.py
+++ vrtual_target/main2.py
@@ -5,6 +5,7 @@ import adafruit_connection_manager
 import adafruit_requests
 import zlib
 import json
+from adafruit_datetime import datetime, date, time, timezone, timedelta
 
 with open(r'vrtual_target/api_key', 'r', encoding='utf-8') as file:
     API_KEY = file.read()
@@ -17,9 +18,9 @@ HEADERS['Accept'] = 'application/json'
 
 # URL for GET request
 JSON_GET_URL = f'https://airapi.airly.eu/v2/measurements/location?locationId=9910&apikey={API_KEY}&Accept-Encoding=gzip'
+requests = adafruit_requests.Session(socket, ssl.create_default_context())
 
 def get_data():
-    requests = adafruit_requests.Session(socket, ssl.create_default_context())
     with requests.get(JSON_GET_URL, headers=HEADERS) as r:
         assert r.status_code == 200, \
             f'Expected 200 but got {r.status_code}'
@@ -32,12 +33,23 @@ def deflate_data(compressed_content):
 
     try:
         data = json.loads(decompressed)
+        iso_date_string = data['current']['tillDateTime']
+        # Adding time offset since hour from sensor is delayed by 2hrs
+        current_time_isodate = datetime.fromisoformat(iso_date_string) + timedelta(hours=2)
+        current_values = data['current']['values']
+
+        text_to_display = ""
+
+        t = current_time_isodate
+        text_to_display += f"{t.hour}:{t.minute} {t.day}.{t.month}.{t.year}\n"
+
+        for item in current_values:
+            text_to_display += f"{item['name']}: {item['value']}\n"
+
+        print(text_to_display)
+
     except Exception as e:
         print(e)
-
-    current_values = data['current']['values']
-    for item in current_values:
-        print(item['name'], item['value'])
 
 
 
